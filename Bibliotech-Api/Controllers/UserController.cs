107aa7761fabdc74a05587fb060ee99811c98268
Zam <samuel.kodehode@gmail.com>
2024-04-15 10:44:51 +0200

    Add POST method to UserController
    The UserController class has been updated to add a new POST method. The method validates the user data before saving it in the context. Additionally, the unnecessary use of the EntityFrameworkCore namespace in the Users model has been removed.

--- Bibliotech-Api/Controllers/UserController.cs
+++ Bibliotech-Api/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using Bibliotech_Api.Access;
+using Bibliotech_Api.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bibliotech_Api.Controllers;
@@ -10,6 +11,19 @@ public class UserController(LocalDbContext context) : Controller
     [HttpGet]
     public IActionResult GetUsers()
     {
-        return Ok(context.users.Where(users => users.id > 5));
+        return Ok(context.users);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostUser([FromBody] Users user)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        context.users.Add(user);
+        await context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetUsers), new { id = user.id }, user);
     }
 }
--- Bibliotech-Api/Models/Users.cs
+++ Bibliotech-Api/Models/Users.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿
 
 namespace Bibliotech_Api.Models;
 
